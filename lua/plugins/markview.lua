a0b06cf3d59224376ec0bee303e667a5d6b4deed
Daniel Gonzalez <daniel.gonzalez10152@gmail.com>
2026-06-08 18:09:04 -0700

    feat: add markview.nvim for markdown rendering

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  dependencies = { "saghen/blink.cmp" },
+  opts = {
+    markdown = {
+      block_quotes = { wrap = false },
+      headings = { org_indent_wrap = false },
+      list_items = { wrap = false },
+    },
+  },
+}
